8908cbb6d9eb15155dc284e6f73d4b8be069d68f
Judson Bandeira <judsonmelobandeira@gmail.com>
2023-02-10 07:52:51 -0300

    first commit

---
+++ web/modules/custom/movie_directory/src/Controller/MovieListing.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Drupal\movie_directory\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+
+class MovieListing extends ControllerBase {
+
+    public function view() {
+
+        $this->listMovies();
+        $content = [];
+
+        $content['name'] = 'My name is Bob';
+
+        return [
+            '#theme' => 'movie-listing',
+            '#content' => $content,
+        ];
+    }
+
+    public function listMovies() {
+        $movie_api_connector_service = \Drupal::service('movie_directory.api_connector');
+        $movie_list = $movie_api_connector_service->discoverMovies();
+        if (!empty($movie_list->results)) {
+            return $movie_list->results;
+        }
+        return [];
+    }
+}
